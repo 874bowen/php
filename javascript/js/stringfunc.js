eb318246716f692377e0df81e48bb7cdcb08df73
Ivan Bowen <ivanovicbow850@gmail.com>
2022-02-15 14:14:19 +0300

    basics in js

---
+++ javascript/js/stringfunc.js
@@ -0,0 +1,11 @@
+// export const capitalizeString = str => str.toUpperCase();
+export const capitalizeString = str => str.toUpperCase();
+export { capitalizeString };
+export const foo = "bar";
+export const bar = "foo";
+//import * meeans import everything from a file
+// import * as capitalizeStrings from "capitalize_strings";
+//fall back export 
+// export default class
+import subtract from "mathfuncs"
+//importing default funcs
